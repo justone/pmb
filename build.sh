e06b04f20bf6e4c029561723965c41506c6b45c2
Nate Jones <nate@endot.org>
2014-04-27 21:57:01 -0700

    build script for master builds

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ABBREV_SHA1=$(git log --format=%h -1)
+DATE=$(date +%Y-%m-%d-%H%M)
+VERSION="${DATE}-${ABBREV_SHA1}"
+
+echo "Building $VERSION"
+echo
+
+
+gox -osarch="darwin/amd64 linux/amd64"
+
+mkdir $VERSION
+
+cp bootstrap.template $VERSION/bootstrap
+perl -p -i -e "s/VERSION/$VERSION/g" $VERSION/bootstrap
+md5sum pmb_* >> $VERSION/bootstrap
+cp pmb_* $VERSION/
