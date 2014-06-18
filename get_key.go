1874baf88ab8b4438435c6f2c7924d8a3274e226
Nate Jones <nate@endot.org>
2014-06-18 06:17:43 -0700

    simple command to just print the secret key
    this is for integration into third party apps

---
+++ get_key.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/justone/pmb/api"
+)
+
+type GetKeyCommand struct {
+	// nothing yet
+}
+
+var getKeyCommand GetKeyCommand
+
+func (x *GetKeyCommand) Execute(args []string) error {
+	bus := pmb.GetPMB(urisFromOpts(globalOptions))
+
+	id := generateRandomID("getKey")
+
+	conn, err := bus.GetConnection(id, false)
+	if err != nil {
+		return err
+	}
+
+	return runGetKey(conn)
+}
+
+func init() {
+	parser.AddCommand("get-key",
+		"Print the secret key to stdout. (low level)",
+		"",
+		&getKeyCommand)
+}
+
+func runGetKey(conn *pmb.Connection) error {
+
+	fmt.Println(conn.Key)
+
+	return nil
+}
