5a42ae1dbf04f74aa09e225cdddb9551446bbff8
Nate Jones <nate@endot.org>
2014-07-04 11:15:15 -0700

    add low level message dump command

---
+++ dump_raw.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/justone/pmb/api"
+)
+
+type DumpRawCommand struct {
+	// nothing yet
+}
+
+var dumpRawCommand DumpRawCommand
+
+func (x *DumpRawCommand) Execute(args []string) error {
+	bus := pmb.GetPMB(urisFromOpts(globalOptions))
+
+	id := generateRandomID("dumpRaw")
+
+	conn, err := bus.GetConnection(id, false)
+	if err != nil {
+		return err
+	}
+
+	return runDumpRaw(conn)
+}
+
+func init() {
+	parser.AddCommand("dump-raw",
+		"Print out each raw JSON message as it comes through. (low level)",
+		"",
+		&dumpRawCommand)
+}
+
+func runDumpRaw(conn *pmb.Connection) error {
+
+	for {
+		message := <-conn.In
+		fmt.Println(message.Raw)
+	}
+
+	return nil
+}
