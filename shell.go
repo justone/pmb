347ff4ee8445cc3c1b16359a9d1abb7f5acf72b4
Nate Jones <nate@endot.org>
2015-12-02 21:48:21 -0800

    new subcommand: shell
    for those times when you want to run multiple commands
    without having to re-enter the key

---
+++ shell.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/justone/pmb/api"
+)
+
+type ShellCommand struct {
+	// nothing yet
+}
+
+var shellCommand ShellCommand
+
+func (x *ShellCommand) Execute(args []string) error {
+	bus := pmb.GetPMB(urisFromOpts(globalOptions))
+
+	id := generateRandomID("shell")
+
+	conn, err := bus.ConnectClient(id, !globalOptions.TrustKey)
+	if err != nil {
+		return err
+	}
+
+	return runShell(conn)
+}
+
+func init() {
+	parser.AddCommand("shell",
+		"Run a subshell with PMB_KEY set, for running multiple commands.",
+		"",
+		&shellCommand)
+}
+
+func runShell(conn *pmb.Connection) error {
+
+	// set the key in the environment
+	os.Setenv("PMB_KEY", strings.Join(conn.Keys, ","))
+
+	// get the shell, defaulting to bash
+	var shell string
+	if shell = os.Getenv("SHELL"); len(shell) == 0 {
+		shell = "/bin/bash"
+	}
+
+	cmd := exec.Command(shell, "-l")
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
