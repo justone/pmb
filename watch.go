d8d054fa1b07dc0fd52ae145a0f939ba31d6c946
Nate Jones <nate@endot.org>
2016-06-07 21:32:21 -0700

    add single file watching

--- watch.go
+++ watch.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 	"strconv"
 	"time"
@@ -12,7 +13,8 @@ import (
 
 type WatchCommand struct {
 	Message string  `short:"m" long:"message" description:"Message to send."`
-	Pid     int     `short:"p" long:"pid" description:"Watch after PID exits."`
+	Pid     int     `short:"p" long:"pid" description:"Notify after PID exits."`
+	File    string  `short:"f" long:"file" description:"Notify after file stops changing."`
 	Level   float64 `short:"l" long:"level" description:"Notification level (1-5), higher numbers indictate higher importance" default:"3"`
 }
 
@@ -21,8 +23,8 @@ var watchCommand WatchCommand
 func (x *WatchCommand) Execute(args []string) error {
 	bus := pmb.GetPMB(globalOptions.Primary)
 
-	if len(watchCommand.Message) == 0 && watchCommand.Pid == 0 {
-		return fmt.Errorf("A message is required")
+	if len(watchCommand.Message) == 0 && watchCommand.Pid == 0 && watchCommand.File == "" {
+		return fmt.Errorf("A message or pid or file is required")
 	}
 
 	// fail fast if pid isn't found
@@ -31,6 +33,11 @@ func (x *WatchCommand) Execute(args []string) error {
 
 		if !found {
 			return fmt.Errorf("Process %d not found.", watchCommand.Pid)
+		}
+	}
+	if len(watchCommand.File) > 0 {
+		if _, err := os.Stat(watchCommand.File); os.IsNotExist(err) {
+			return fmt.Errorf("File %s not found.", watchCommand.File)
 		}
 	}
 
@@ -77,6 +84,24 @@ func runWatch(conn *pmb.Connection, id string) error {
 
 		if len(message) == 0 {
 			message = fmt.Sprintf("Command [%s] completed.", watchExecutable)
+		}
+	}
+	if len(watchCommand.File) > 0 {
+		var prevSize int64
+		prevSize = -1
+		for {
+			statInfo, _ := os.Stat(watchCommand.File)
+			if statInfo.Size() == prevSize {
+				logrus.Infof("File stabilized.")
+				break
+			} else {
+				prevSize = statInfo.Size()
+				time.Sleep(5 * time.Second)
+			}
+		}
+
+		if len(message) == 0 {
+			message = fmt.Sprintf("File [%s] stabilized.", watchCommand.File)
 		}
 	}
 
